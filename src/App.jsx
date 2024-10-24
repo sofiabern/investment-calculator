6031b021cb1c1f8b5effc1b0f192bc5fec1d0d10
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-24 14:35:13 +0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import UserInput from "./components/UserInput/UserInput";
+import Results from "./components/Results/Results";
 function App() {
+  const[userInput, setUserInput] = useState({initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+})
+
+const inputIsValid = userInput.duration >=1;
+
+function handleChange(inputIndetifier, newValue){
+  setUserInput(prevUserInput =>{
+      return{
+          ...prevUserInput,
+          [inputIndetifier]: +newValue
+      };
+  })
+      }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <UserInput onChange={handleChange} userInput={userInput}/>
+     {!inputIsValid &&  <p className="center">Please enter a duration greater than zero</p>}
+     {inputIsValid &&  <Results input={userInput}/>}
+    </>
+  );
 }
 
-export default App
+export default App;
